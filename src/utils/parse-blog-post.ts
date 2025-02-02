c2eba53919aa95cc639a5cb43d1c29312679b6ad
pheggeseth <pheggeseth@gmail.com>
2025-02-02 08:11:13 -0600

    remove console log

--- src/utils/parse-blog-post.ts
+++ src/utils/parse-blog-post.ts
@@ -4,7 +4,6 @@ import { readBlogPostFile } from './read-blog-post-file';
 export async function parseBlogPost(slug: string) {
 	const { data, content } = await readBlogPostFile(slug);
 	const Component = await createMDXContent(content);
-	console.log({ data, content });
 
 	return {
 		data,
