22c5032df6d4600a1953dc4e284cb112d9a907e2
pheggeseth <pheggeseth@gmail.com>
2025-02-03 22:54:51 -0600

    separate day into function

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -19,5 +19,9 @@ const pluralRules = new Intl.PluralRules('en-us', { type: 'ordinal' });
  * such as `November 22nd, 1985`
  */
 export function formatDate([year, month, day]: PostDate) {
-	return `${new Intl.DateTimeFormat('en-us', { month: 'long' }).format(new Date(year, month - 1, day))} ${day}${ordinalMap[pluralRules.select(day)]}, ${year}` as const;
+	return `${formatDay([year, month, day])}, ${year}` as const;
+}
+
+export function formatDay([year, month, day]: PostDate) {
+	return `${new Intl.DateTimeFormat('en-us', { month: 'long' }).format(new Date(year, month - 1, day))} ${day}${ordinalMap[pluralRules.select(day)]}`;
 }
