678e2cb5d7c34a3f8396a84b4a78b3c31654f0f1
pheggeseth <pheggeseth@gmail.com>
2025-02-03 20:47:55 -0600

    update component name

--- src/pages/thoughts/[slug].tsx
+++ src/pages/thoughts/[slug].tsx
@@ -2,7 +2,7 @@ import { BlogPost } from '../../components/blog-post';
 import type { GetConfig } from '../../types';
 import { parseBlogPost } from '../../utils/parse-blog-post';
 
-export default async function Thoughts({ slug }: { slug: string }) {
+export default async function BlogPostPage({ slug }: { slug: string }) {
 	const { data, Component } = await parseBlogPost(slug);
 
 	return (
