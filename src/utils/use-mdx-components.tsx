49c7529d295f9647cc5c35cec702c03aaf221434
pheggeseth <pheggeseth@gmail.com>
2025-02-12 18:00:01 -0800

    simplify MDX component usage by directly referencing Code component

--- src/utils/use-mdx-components.tsx
+++ src/utils/use-mdx-components.tsx
@@ -1,8 +1,7 @@
-import type { ComponentProps } from 'react';
 import { Code } from '../components/ui/code';
 
 export function useMDXComponents() {
 	return {
-		pre: (props: ComponentProps<typeof Code>) => <Code {...props} />,
+		pre: Code,
 	};
 }
