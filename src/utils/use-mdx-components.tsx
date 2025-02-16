9bd10ee76e3cde3a5ca9e5b5cf3cea905fd9589e
pheggeseth <pheggeseth@gmail.com>
2025-02-16 14:53:50 -0600

    add typing to useMDXComponents function

--- src/utils/use-mdx-components.tsx
+++ src/utils/use-mdx-components.tsx
@@ -1,7 +1,6 @@
+import type { UseMdxComponents } from '@mdx-js/mdx';
 import { Code } from '../components/ui/code';
 
-export function useMDXComponents() {
-	return {
-		pre: Code,
-	};
-}
+export const useMDXComponents: UseMdxComponents = () => ({
+	pre: Code,
+});
