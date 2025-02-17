fef576fc1ae27fb1dd3efb92302214fa1a18e001
pheggeseth <pheggeseth@gmail.com>
2025-02-17 08:30:28 -0600

    reset postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,16 +2,6 @@
 export default {
 	plugins: {
 		/** @type {import('autoprefixer').Options} */
-		autoprefixer: {
-			/* This prevents a React hydration mismatch error in the "Code" component, 
-			as that component internally renders a <style> element 
-			whose content includes a ::selection pseudo selector.
-			That selector gets prefixed by Waku during server render using autoprefixer, 
-			which adds the ::-moz-selection selector.
-			Upon client hydration, this is compared to the base ::selection selector, which results in a mismatch.
-			See https://github.com/code-hike/bright/issues/39 for more details.
-			*/
-			add: false,
-		},
+		autoprefixer: {},
 	},
 };
