559294fa0de04bac80ffc44c4e7b0022e459bdd6
pheggeseth <pheggeseth@gmail.com>
2025-02-18 22:51:16 -0600

    make line numbers sticky

--- src/components/ui/code/code.tsx
+++ src/components/ui/code/code.tsx
@@ -1,4 +1,5 @@
 import './code.css';
+import clsx from 'clsx';
 import {
 	type AnnotationHandler,
 	Pre,
@@ -11,20 +12,26 @@ import { theme } from './theme';
 
 export async function Code({ codeblock }: { codeblock: RawCode }) {
 	const highlighted = await highlight(codeblock, theme);
-
 	const handlers: AnnotationHandler[] = [];
+	let hasLineNumbers = false;
+
 	if (
 		!highlighted.annotations.some(
 			(a) => a.name === 'line-numbers' && a.query === 'false',
 		)
 	) {
+		hasLineNumbers = true;
 		handlers.push(lineNumbers);
 	}
 
 	return (
 		<>
 			<SelectionStyle theme={theme} />
-			<Pre className="pre" code={highlighted} handlers={handlers} />
+			<Pre
+				className={clsx('pre', hasLineNumbers && 'line-numbers')}
+				code={highlighted}
+				handlers={handlers}
+			/>
 		</>
 	);
 }
