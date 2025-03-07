a91f38bc5b2cebe561ad49e0b5405e26a74f05f7
pheggeseth <pheggeseth@gmail.com>
2025-03-07 09:25:48 -0600

    add Copy component for clipboard functionality with feedback

---
+++ src/components/ui/copy/copy.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import { Check, Clipboard } from 'react-feather';
+
+export function Copy() {
+	const [copied, setCopied] = useState<false | object>(false);
+
+	function handleSuccess() {
+		setCopied({});
+	}
+
+	function handleError() {
+		console.error('Failed to copy text.');
+	}
+
+	function handleCopyWithFeedback() {
+		navigator.clipboard
+			.writeText('Hello, world!')
+			.then(handleSuccess)
+			.catch(handleError);
+	}
+
+	const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
+
+	useEffect(() => {
+		if (timeoutIdRef.current) {
+			clearTimeout(timeoutIdRef.current);
+		}
+
+		if (copied) {
+			timeoutIdRef.current = setTimeout(() => {
+				setCopied(false);
+			}, 1500);
+		}
+
+		return () => {
+			if (timeoutIdRef.current) {
+				clearTimeout(timeoutIdRef.current);
+			}
+		};
+	}, [copied]);
+
+	return (
+		<div>
+			<button
+				type="button"
+				onClick={handleCopyWithFeedback}
+				style={{ padding: 4 }}
+			>
+				{copied ? (
+					<Check style={{ height: 16, width: 16, display: 'block' }} />
+				) : (
+					<Clipboard style={{ height: 16, width: 16, display: 'block' }} />
+				)}
+			</button>
+		</div>
+	);
+}
